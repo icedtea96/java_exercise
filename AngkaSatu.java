1d15f7a0c89d5a9dd7a0f1088b759bd7eeec3fd3
Nurudin Ma'ruf <nurudinmaruf09@gmail.com>
2023-09-24 19:48:28 +0700

    Add files

--- AngkaSatu.java
+++ AngkaSatu.java
@@ -1,12 +1,18 @@
 package MyOwn;
 
+import java.util.Scanner;
+
 public class AngkaSatu {
     public static void main(String[] args) {
-        int n = 3;
+		Scanner input = new Scanner(System.in);
+		System.out.print("Masukkan nilai n: ");
+        int n = input.nextInt();
+
 		int start = 100;
 		int lompat = 3;
 		
 		int hasilK = lompat;
+		System.out.println(start + " adalah si angka 1");
 		for (int i = 1; i <= n; i++) {
 			
 			System.out.println((start+hasilK) + " adalah si angka 1");
@@ -18,5 +24,7 @@ public class AngkaSatu {
 			}
 
 		}
+
+		input.close();
     }
 }
